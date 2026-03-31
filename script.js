f98a3874ef149c24e48d5370b255bfef9d70c24a
Yu <86412517+aiiewtpwypo@users.noreply.github.com>
2026-03-31 22:42:21 +0800

    Create script.js by extracting JavaScript from index.html including fixed deleteEvent function

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code extracted from index.html
+
+function deleteEvent(eventId) {
+    // Logic for deleting the event goes here.
+    console.log(`Deleting event with ID: ${eventId}`);
+}
+
+// Other JavaScript code...
+// Place any additional functions or code that were present in index.html here.
